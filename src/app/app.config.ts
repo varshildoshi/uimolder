d5998e2c05413fdc9266e3cc2db8056cba957984
varshildoshi <varshil.mscit16@gmail.com>
2026-02-03 18:29:29 +0530

    tailwind added to stage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCk2rj6rfS9nn9WU1Vsk4_xq9cWfb9h52k",
+    authDomain: "uimolder.firebaseapp.com",
+    projectId: "uimolder",
+    storageBucket: "uimolder.firebasestorage.app",
+    messagingSenderId: "115575908136",
+    appId: "1:115575908136:web:7fca5d1ae4ac4b610d521d"
+  }
+};
