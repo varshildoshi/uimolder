7db417bf4fa8e5f3d0af0b2b4af43bd2d2010b0a
varshildoshi <varshil.mscit16@gmail.com>
2026-02-24 15:41:56 +0530

    forge-builder deleted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-// import { ForgeBuilder } from './components/forge-builder/forge-builder';
-import { ForgeBuilderV2 } from './components/forge-builder-v2/forge-builder-v2';
-import { ForgeBuilder } from './components/forge-builder/forge-builder';
 import { LayoutBuilder } from './components/layout-builder/layout-builder';
 import { PageNotFoundComponent } from './components/404/404';
 
@@ -12,8 +9,6 @@ export const routes: Routes = [
         component: Home,
         title: 'UiMolder | Architecting High-End UI'
     },
-    { path: 'builder', component: LayoutBuilder }, // The New Builder
-    { path: 'builder-v1', component: ForgeBuilder },
-    { path: 'builder-v2', component: ForgeBuilderV2 },
+    { path: 'builder', component: LayoutBuilder }, // The Layout Builder
     { path: '**', component: PageNotFoundComponent }
 ];
